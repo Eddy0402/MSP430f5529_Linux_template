354249b709624b8f4622fac799a0e284a3a7c060
Eddy Wu <yukkcake@gmail.com>
2015-11-25 01:32:00 +0800

    Minified path variables need to be configure.

--- mk/toolchain.mk
+++ mk/toolchain.mk
@@ -1,13 +1,14 @@
-GCC_DIR =  ~/ti/gcc/bin
-SUPPORT_FILE_DIRECTORY = ~/ti/gcc/include/
-CC             = $(GCC_DIR)/msp430-elf-gcc
-OBJCOPY        = $(GCC_DIR)/msp430-elf-objcopy
-GDB            = $(GCC_DIR)/msp430-elf-gdb
-GDB_SERVER     = $(GCC_DIR)/gdb_agent_console
-GDB_SERVER_DAT = $(GCC_DIR)/../msp430.dat
-# absolute path because of running root
+GCC_DIR =  ~/ti/gcc
+SUPPORT_FILE_DIRECTORY = $(GCC_DIR)/include/
+CC             = $(GCC_DIR)/bin/msp430-elf-gcc
+OBJCOPY        = $(GCC_DIR)/bin/msp430-elf-objcopy
+GDB            = $(GCC_DIR)/bin/msp430-elf-gdb
+GDB_SERVER     = $(GCC_DIR)/bin/gdb_agent_console
+GDB_SERVER_DAT = $(GCC_DIR)/msp430.dat
+
+# absolute path because of running as root
 FLASH_LIBDIR = /home/eddy/ti/MSP430Flasher_1.3.7
-FLASH        = /home/eddy/ti/MSP430Flasher_1.3.7/MSP430Flasher
+FLASH        = $(FLASH_LIBDIR)/MSP430Flasher
 
 DEVICE       = msp430f5529
 
